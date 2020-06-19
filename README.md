96698d055d866d6f8d804ffdbc72f8f77e3aa7a1
Fernando Veras Machado <fernando.veras@live.com>
2020-06-19 16:06:33 -0300

    Adicionando as funcionalidades do front-end

---
+++ README.md
@@ -0,0 +1,37 @@
+# GOSTACK - Conceitos do React JS
+
+![](https://img.shields.io/badge/made%20by-fernmac-04d361?style=flat&color=04d361) 
+![](https://img.shields.io/github/languages/count/fernmac/gostack-conceitos-reactjs?style=flat&color=04d361) 
+[![GitHub stars](https://img.shields.io/github/stars/fernmac/gostack-conceitos-reactjs?style=social)](https://github.com/fernmac/gostack-conceitos-reactjs/stargazers)
+
+Aplicação para armazenar repositórios, que irá permitir a criação, listagem, atualização e remoção dos repositórios, e além disso permitir que os repositórios possam receber "likes".
+
+------------
+
+## Instalação
+
+- Para iniciar a instalação é necessário clonar o repositório
+    >`git clone [url_repositorio]`
+
+- Entrar no diretório clonado
+    >`cd gostack-conceitos-reactjs`
+
+- Instalar as dependências do projeto via **NPM** ou **YARN**
+    >`npm install`
+
+- Essa aplicação depende do back-end criado no desafio anterior: **[gostack-conceitos-nodejs](https://github.com/fernmac/gostack-conceitos-nodejs)**
+
+## Execução
+
+- Executar o comando para iniciar o front-end
+    >`npm run start`
+
+## Funcionalidades
+
+O front-end tem o objetivo de fornecer as seguintes funcionalidades:
+
+- Listar os repositórios
+
+- Adicionar um repositório
+
+- Remover um repositório
--- src/App.js
+++ src/App.js
@@ -1,31 +1,95 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
+	const [repositories, setRepositories] = useState([]);
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+	const [formData, setFormData] = useState({ title: "", url: "", techs: "" });
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+	useEffect(() => {
+		api.get("/repositories").then((response) => {
+			if (!response.data.error) setRepositories(response.data);
+		});
+	}, []);
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+	function handleInputChange(event) {
+		const { name, value } = event.target;
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+		setFormData({ ...formData, [name]: value });
+	}
+
+	async function handleAddRepository(event) {
+		event.preventDefault();
+
+		const { title, url, techs } = formData;
+
+		const response = await api.post("/repositories", {
+			title,
+			url,
+			techs: techs.split(","),
+		});
+
+		setRepositories([...repositories, response.data]);
+	}
+
+	async function handleRemoveRepository(id) {
+		await api.delete(`/repositories/${id}`);
+
+		setRepositories(repositories.filter((repository) => repository.id !== id));
+	}
+
+	async function handleLikeRepository(id) {
+		await api.post(`/repositories/${id}/like`);
+	}
+
+	return (
+		<>
+			<ul data-testid="repository-list">
+				{repositories.map((repository) => (
+					<li key={repository.id}>
+						{repository.title}
+						<button onClick={() => handleLikeRepository(repository.id)}>
+							Like!
+						</button>
+						<button onClick={() => handleRemoveRepository(repository.id)}>
+							Remover
+						</button>
+					</li>
+				))}
+			</ul>
+
+			<form>
+				<input
+					type="text"
+					name="title"
+					id="title"
+					placeholder="Nome do Repositrio"
+					onChange={handleInputChange}
+				/>
+				<input
+					type="text"
+					name="url"
+					id="url"
+					placeholder="Url do Repositório"
+					onChange={handleInputChange}
+				/>
+				<input
+					type="text"
+					name="techs"
+					id="techs"
+					placeholder="Tecnologias (separar por vírgulas)"
+					onChange={handleInputChange}
+				/>
+
+				<button type="submit" onClick={handleAddRepository}>
+					Adicionar
+				</button>
+			</form>
+		</>
+	);
 }
 
 export default App;
